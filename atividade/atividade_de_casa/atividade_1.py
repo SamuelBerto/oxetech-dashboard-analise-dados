0a6ce1f1b1a3303aa33278b21156daf3c25fc383
Samuel Berto <samuztberto@gmail.com>
2026-05-28 23:38:37 -0300

    feat: adiciona atividade 01

---
+++ atividade/atividade_de_casa/atividade_1.py
@@ -0,0 +1,14 @@
+# Atividade de Casa 01 - Exercício 01
+
+#int() converte o valor digitado para número inteiro
+numero_inteiro = int(input("Digite um número inteiro: "))
+
+
+numero_inteiro_2 = int(input("Digite o segundo número inteiro: "))
+
+#Exibindo os resultados das operações matemáticas na tela
+print(f"Soma: {numero_inteiro + numero_inteiro_2}")
+print(f"Subtração: {numero_inteiro - numero_inteiro_2}")
+print(f"Multiplicação: {numero_inteiro * numero_inteiro_2}")
+print(f"Divisão: {numero_inteiro / numero_inteiro_2}")
+                    
