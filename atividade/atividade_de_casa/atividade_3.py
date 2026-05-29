eaa21b55fe28a66fce067b789a7c34cf4c18c695
Samuel Berto <samuztberto@gmail.com>
2026-05-29 00:53:22 -0300

    feat: adiciona atividade sobre tipos de dados

---
+++ atividade/atividade_de_casa/atividade_3.py
@@ -0,0 +1,48 @@
+# Atividade de Casa 3 - Tipos de Dados.
+
+# 1) Qual é o tipo de cada variável?
+
+# a = "Python"
+
+# Resposta: a é do tipo string (str)(texto).
+
+# b = 2025
+
+# Resposta: b é do tipo inteiro (int)(número inteiro).
+
+# c = 3.14
+
+# Resposta: c é do tipo float (número decimal).
+
+# d = True
+
+# Resposta: d é do tipo booleano (bool), usado para valores True ou False (verdadeiro ou falso).
+
+# 2) Quais podem ser usadas em um if?
+
+# Resposta: Todas podem ser usadas no if.
+
+# EXEMPLOS:
+# Variáveis
+a = "Python"
+b = 2025
+c = 3.14
+d = True
+
+# Verificando a variável "a"
+if a:
+    print("A variável 'a' é verdadeira.")
+
+# Verificando a variável "b"
+if b:
+    print("A variável 'b' é verdadeira.")
+
+# Verificando a variável "c"
+if c:
+    print("A variável 'c' é verdadeira.")
+
+# Verificando a variável "d"
+if d:
+    print("A variável 'd' é verdadeira.")
+
+# O if serve para verificar uma condição e executar um bloco de código se a condição for verdadeira. No exemplo acima, todas as variáveis são consideradas verdadeiras, então todas as mensagens serão impressas.  
