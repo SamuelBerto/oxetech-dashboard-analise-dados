788464cc598457d8380edfb323ed8ecde2f4c453
Samuel Berto <samuztberto@gmail.com>
2026-05-28 21:52:43 -0300

    feat: adiciona exercício 03

---
+++ exercicios/exerc-03.py
@@ -0,0 +1,8 @@
+largura = float(input("Digite a largura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print(f"Área do retângulo: {area}")
+print(f"Perímetro do retângulo: {perimetro}")
